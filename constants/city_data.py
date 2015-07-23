e5c0d0ba7c4d468c1fbaab2468d8dfdefe3384df
wocoburguesa <wocoburguesa@gmail.com>
2015-07-23 00:44:44 -0500

    Added proxy functionality

---
+++ constants/city_data.py
@@ -0,0 +1,327 @@
+city_data = {
+    'NM': [
+        {
+            'name': 'Albuquerque',
+            'woeid': 2352824
+            },
+        ],
+    'GA': [
+        {
+            'name': 'Atlanta',
+            'woeid': 2357024
+            }
+        ],
+    'TX': [
+        {
+            'name': 'Austin',
+            'woeid': 2357536
+            },
+        {
+            'name': 'Dallas-Ft. Worth',
+            'woeid': 2388929
+            },
+        {
+            'name': 'El Paso',
+            'woeid': 2397816
+            },
+        {
+            'name': 'Houston',
+            'woeid': 2424766
+            },
+        {
+            'name': 'San Antonio',
+            'woeid': 2487796
+            }
+        ],
+    'MD': [
+        {
+            'name': 'Baltimore',
+            'woeid': 2358820
+            }
+        ],
+    'LA': [
+        {
+            'name': 'Baton Rouge',
+            'woeid': 2359991
+            },
+        {
+            'name': 'New Orleans',
+            'woeid': 2458833
+            }
+        ],
+    'AL': [
+        {
+            'name': 'Birmingham',
+            'woeid': 2364559
+            }
+        ],
+    'MA': [
+        {
+            'name': 'Boston',
+            'woeid': 2367105
+            }
+        ],
+    'NC': [
+        {
+            'name': 'Charlotte',
+            'woeid': 2378426
+            },
+        {
+            'name': 'Greensboro',
+            'woeid': 2414469
+            },
+        {
+            'name': 'Raleigh',
+            'woeid': 2478307
+            }
+        ],
+    'IL': [
+        {
+            'name': 'Chicago',
+            'woeid': 2379574
+            }
+        ],
+    'OH': [
+        {
+            'name': 'Cincinnati',
+            'woeid': 2380358
+            },
+        {
+            'name': 'Cleveland',
+            'woeid': 2381475
+            },
+        {
+            'name': 'Columbus',
+            'woeid': 2383660
+            }
+        ],
+    'CO': [
+        {
+            'name': 'Colorado Springs',
+            'woeid': 2383489
+            },
+        {
+            'name': 'Denver',
+            'woeid': 2391279
+            }
+        ],
+    'MI': [
+        {
+            'name': 'Detroit',
+            'woeid': 2391585
+            }
+        ],
+    'CA': [
+        {
+            'name': 'Fresno',
+            'woeid': 2407517
+            },
+        {
+            'name': 'Long Beach',
+            'woeid': 2441472
+            },
+        {
+            'name': 'Los Angeles',
+            'woeid': 2442047
+            },
+        {
+            'name': 'Sacramento',
+            'woeid': 2486340
+            },
+        {
+            'name': 'San Diego',
+            'woeid': 2487889
+            },
+        {
+            'name': 'San Francisco',
+            'woeid': 2487956
+            },
+        {
+            'name': 'San Jose',
+            'woeid': 2488042
+            }
+        ],
+    'PA': [
+        {
+            'name': 'Harrisburg',
+            'woeid': 2418046
+            },
+        {
+            'name': 'Philadelphia',
+            'woeid': 2471217
+            },
+        {
+            'name': 'Pittsburgh',
+            'woeid': 2473224
+            }
+        ],
+    'HI': [
+        {
+            'name': 'Honolulu',
+            'woeid': 2423945
+            }
+        ],
+    'IN': [
+        {
+            'name': 'Indianapolis',
+            'woeid': 2427032
+            }
+        ],
+    'MS': [
+        {
+            'name': 'Jackson',
+            'woeid': 2428184
+            }
+        ],
+    'FL': [
+        {
+            'name': 'Jacksonville',
+            'woeid': 2428344
+            },
+        {
+            'name': 'Miami',
+            'woeid': 2450022
+            },
+        {
+            'name': 'Orlando',
+            'woeid': 2466256
+            },
+        {
+            'name': 'Tallahassee',
+            'woeid': 2503713
+            },
+        {
+            'name': 'Tampa',
+            'woeid': 2503863
+            }
+        ],
+    'MO': [
+        {
+            'name': 'Kansas City',
+            'woeid': 2430683
+            },
+        {
+            'name': 'St. Louis',
+            'woeid': 2486982
+            }
+        ],
+    'NV': [
+        {
+            'name': 'Las Vegas',
+            'woeid': 2436704
+            }
+        ],
+    'KY': [
+        {
+            'name': 'Louisville',
+            'woeid': 2442327
+            }
+        ],
+    'TN': [
+        {
+            'name': 'Memphis',
+            'woeid': 2449323
+            },
+        {
+            'name': 'Nashville',
+            'woeid': 2457170
+            }
+        ],
+    'AZ': [
+        {
+            'name': 'Mesa',
+            'woeid': 2449808
+            },
+        {
+            'name': 'Phoenix',
+            'woeid': 2471390
+            },
+        {
+            'name': 'Tucson',
+            'woeid': 2508428
+            }
+        ],
+    'WI': [
+        {
+            'name': 'Milwaukee',
+            'woeid': 2451822
+            }
+        ],
+    'MN': [
+        {
+            'name': 'Minneapolis',
+            'woeid': 2452078
+            }
+        ],
+    'CT': [
+        {
+            'name': 'New Haven',
+            'woeid': 2458410
+            }
+        ],
+    'NY': [
+        {
+            'name': 'New York',
+            'woeid': 2459115
+            }
+        ],
+    'VA': [
+        {
+            'name': 'Norfolk',
+            'woeid': 2460389
+            },
+        {
+            'name': 'Richmond',
+            'woeid': 2480894
+            },
+        {
+            'name': 'Virginia Beach',
+            'woeid': 2512636
+            }
+        ],
+    'OK': [
+        {
+            'name': 'Oklahoma City',
+            'woeid': 2464592
+            }
+        ],
+    'NE': [
+        {
+            'name': 'Omaha',
+            'woeid': 2465512
+            }
+        ],
+    'OR': [
+        {
+            'name': 'Portland',
+            'woeid': 2475687
+            }
+        ],
+    'RI': [
+        {
+            'name': 'Providence',
+            'woeid': 2477058
+            }
+        ],
+    'UT': [
+        {
+            'name': 'Salt Lake City',
+            'woeid': 2487610
+            }
+        ],
+    'WA': [
+        {
+            'name': 'Seattle',
+            'woeid': 2490383
+            }
+        ],
+    'DC': [
+        {
+            'name': 'Washington',
+            'woeid': 2514815
+            }
+        ]
+    }
+
+def __init__(self):
+    return
--- world-tweets-proxy.py
+++ world-tweets-proxy.py
@@ -1,11 +1,64 @@
-import cherrypy
 import os
-class HelloWorld(object):
+import json
+import random
+import requests
+import string
+
+import cherrypy
+
+from constants import city_data
+
+twitter_bearer_token = 'AAAAAAAAAAAAAAAAAAAAAADqggAAAAAADaXS3JkZSt5m6RRmJ0dXZNkE7%2BE%3DOCNTnvPlh7UVYtJkhZDkjjKDIEeqI4IoqD3SBlmj5SyEeNt4Ca'
+yahoo_client_id = 'dj0yJmk9bE5ORHo2MFpJR0NoJmQ9WVdrOVJqbHpOMXBTTTJVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1iZQ--'
+
+class WorldTweetsProxy(object):
+    @cherrypy.expose
     def index(self):
-        return "Hello World!"
-    index.exposed = True
+        return "wocoburguesa, 2015"
+
+    @cherrypy.expose
+    def places_proxy(self, place=None):
+        if place:
+            twitter_headers = {
+                'Authorization': 'Bearer ' + twitter_bearer_token
+                }
+#            r = requests.get(
+#                'http://where.yahooapis.com/v1/places.q' +
+#                "('" + place + "')?" +
+#                'appid=' + yahoo_client_id +
+#                '&format=json'
+#                )
+#            yahoo_response = json.loads(r.text)
+#            print r.text
+#            woeid = yahoo_response['places']['place'][0]['woeid']
+
+#            r = requests.get(
+#                'https://api.twitter.com/1.1/trends/available.json',
+#                headers=twitter_headers
+#                )
+#            places = json.loads(r.text)
+#            us_places = []
+#            for place in places:
+#                print place['country']
+#                if place['country'] == 'United States':
+#                    us_places.append(place)
+#            return json.dumps(us_places)
+            print city_data.city_data
+            woeid = city_data.city_data[place][0]['woeid']
+
+            twitter_url = 'https://api.twitter.com/1.1/trends/place.json?id=' +\
+                str(woeid)
+            print twitter_url
+
+            r = requests.get(
+                twitter_url,
+                headers=twitter_headers
+                )
+            return r.text
+        else:
+            return ''
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0',})
     cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
-    cherrypy.quickstart(HelloWorld())
+    cherrypy.quickstart(WorldTweetsProxy())
